c77c35a8e5b5d7f57a11788ab512a5bcc40bee27
nmjd <spectrermn@gmail.com>
2024-03-29 18:23:39 +0100

    update

--- commande.js
+++ commande.js
@@ -1,26 +1,26 @@
 
-//form commande
+// //form commande
 
-let commandeForm = document.getElementById("commandeForm");
+// let commandeForm = document.getElementById("commandeForm");
 
-commandeForm.addEventListener("submit", (e) => {
-  e.preventDefault();
+// commandeForm.addEventListener("submit", (e) => {
+//   e.preventDefault();
 
-  let item = document.getElementById("itemSelect");
-  let quantity = document.getElementById("quantityInput");
-  let details = document.getElementById("detailsTextarea");
-  let name = document.getElementById("nameInput");
-  let phone = document.getElementById("phone");
-  let mail = document.getElementById("emailInput");
+//   let item = document.getElementById("itemSelect");
+//   let quantity = document.getElementById("quantityInput");
+//   let details = document.getElementById("detailsTextarea");
+//   let name = document.getElementById("nameInput");
+//   let phone = document.getElementById("phone");
+//   let mail = document.getElementById("emailInput");
 
-  if (name.value == "" || phone.value == "", item.value == "", quantity.value == "") {
-    alert("Veuillez remplir les informations manquantes !");
-  } else {
-    console.log(
-      `Commande: ${item.value}, ${quantity.value}, ${details.value}, ${mail.value}, ${phone.value}`
-    );
+//   if (name.value == "" || phone.value == "", item.value == "", quantity.value == "") {
+//     alert("Veuillez remplir les informations manquantes !");
+//   } else {
+//     console.log(
+//       `Commande: ${item.value}, ${quantity.value}, ${details.value}, ${mail.value}, ${phone.value}`
+//     );
     
-    window.open(`https://wa.me/+242055105490/?text=Commande de mr/mme ${name.value} : ${quantity.value} ${item.value} \n Adresse mail : ${mail.value} \n Téléphone : ${phone.value} \n Quelques détails supplémentaires: ${details.value}`);
-    // alert("Votre commande a été envoyée avec succès");
-  }
-});
+//     window.open(`https://wa.me/+242055105490/?text=Commande de mr/mme ${name.value} : ${quantity.value} ${item.value} \n Adresse mail : ${mail.value} \n Téléphone : ${phone.value} \n Quelques détails supplémentaires: ${details.value}`);
+//     // alert("Votre commande a été envoyée avec succès");
+//   }
+// });
